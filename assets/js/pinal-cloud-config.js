c1305056cd2edd15cda462aca206fa5613257eb4
pinalworkforce1-del <pinal.workforce1@gmail.com>
2026-09-04 20:17:09 -0700

    Add Pinal BOOST cloud configuration

---
+++ assets/js/pinal-cloud-config.js
@@ -0,0 +1,6 @@
+window.PINAL_BOOST_CONFIG = Object.freeze({
+  url: 'https://dxcajwarqojvmbteroco.supabase.co',
+  key: 'sb_publishable_ehUKOOksq5SlkTNb-wQ8ZA_Zh8XlWDF',
+  region: 'Pinal County',
+  authRedirect: 'https://pinalworkforce1-del.github.io/BOOST_Pathway-Portal/'
+});
