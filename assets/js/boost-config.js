73cc56b4a3563b24f8645b3a4f34b96716457e19
pinalworkforce1-del <pinal.workforce1@gmail.com>
2026-09-07 16:46:48 -0700

    Add shared Supabase configuration for Pinal BOOST

---
+++ assets/js/boost-config.js
@@ -0,0 +1,6 @@
+window.BOOST_CONFIG = {
+  cloudEnabled: true,
+  supabaseUrl: "https://dxcajwarqojvmbteroco.supabase.co",
+  supabaseAnonKey: "sb_publishable_ehUKOOksq5SlkTNb-wQ8ZA_Zh8XlWDF",
+  region: "Pinal County"
+};
